37ac036658b065102a05020864b604277c4fd9e5
leanalvarez <leanalvarez.dev@gmail.com>
2024-06-21 17:32:26 -0300

    bd

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 
+import {pool} from './db.js'
+
 const app = express()
 
+app.get("/ping", async(req, res) => {
+    const [result] = await pool.query("SELECT 1 + 1 AS result")
+    res.json(result[0])
+})
 
 app.get("/Ios", (req,res) => res.send('Obteniendo los proyectos ios'))
 
